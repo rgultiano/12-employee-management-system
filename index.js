d8483d8632b59b0f2fe034e1bb9c9fed4bc6861c
Ron G <62861315+rgultiano@users.noreply.github.com>
2021-10-17 16:50:17 +1100

    Finished up code

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ async function init(){
                 {value: 'view;department', name: 'View all departments'},
                 {value: 'view;role', name: 'View all roles'},
                 {value: 'view;employee', name: 'View all employees'},
+                {value: 'add;department', name: 'Add department'},
+                {value: 'add;role', name: 'Add role'},
+                {value: 'add;employee', name: 'Add employee'},
                 {value: 'End', name: 'Exit'},
             ],
         }]);
@@ -39,15 +42,85 @@ async function init(){
         if(response.choice === 'End'){
                 finish = true;
         } else if(response.choice.split(';')[0] === 'view'){
-            printTable(response.choice.split(';')[1]);
+            switch(response.choice.split(';')[1]){
+                case 'department':
+                    printTable(`SELECT * FROM department`);
+                    await sleep(500);
+                    break;
+                case 'role':
+                    printTable(`SELECT role.id, role.title, role.salary, department.name as department FROM role left outer join department on role.department_id = department.id`);
+                    await sleep(500);
+                    break;
+                case 'employee':
+                    printTable(`SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, concat(manager.first_name, ' ',  manager.last_name) as manager FROM employee left outer join role on employee.role_id = role.id left outer join department on role.department_id = department.id left outer join employee manager on employee.manager_id = manager.id`);
+                    await sleep(500);
+                    break;
+            }
             // wait a bit to give time for console.table to print before next prompt
             await sleep(500);
+        } else if(response.choice.split(';')[0] === 'add'){
+            switch(response.choice.split(';')[1]){
+                case 'department':
+                    await addTable('department', [{name: 'name'}]);
+                    await sleep(500);
+                    break;
+                case 'role':
+                    await addTable('role', [{name: 'title'}, {name: 'salary', type: 'number'}, {name: 'department_id'}]);
+                    await sleep(500);
+                    break;
+                case 'employee':
+                    await addTable('employee', [{name: 'first_name'}, {name: 'last_name'}, {name: 'role_id'}, {name: 'manager_id'}]);
+                    await sleep(500);
+                    break;
+            }
         }
     }
 }
 
- function printTable(table_name){
-    db.query(`SELECT * FROM ${table_name}`, function (err, results) {
+function printTable(sql){
+    db.query(sql, function (err, results) {
         console.table(results);
-      });
-  }
+        });
+}
+
+async function addTable(table_name, columns){
+    // inquire about each column value
+    const questions = []
+    columns.forEach(column=>{
+        questions.push(    
+            {
+            type: column.type || 'input',
+            message: `${column.name}:`,
+            name: column.name,
+            }
+        );
+    });
+    response = await inquirer.prompt(questions);
+
+    let sql = `INSERT INTO ${table_name} (`;
+    sql_val = '';
+    const vals = [];
+    let first = true;
+    for(key in response){
+        if(!first){
+            sql += ', ';
+            sql_val += ', ';
+        }
+        first = false;
+
+        sql +=  `${key}`
+        sql_val += '?';
+        vals.push(response[key])
+    }
+    sql += `) VALUES (${sql_val})`
+    
+    console.log(response);
+
+    db.query(sql, vals, function (err, results) {
+        if(err){
+            console.log(err);
+        }
+        console.log(results);
+    });
+    
+}
